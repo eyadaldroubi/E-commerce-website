c5e1795cd7ff7ada9863aeb49054ae23fd4ccd5f
EYAD <eyadaldroubi@gmail.com>
2026-04-22 18:15:29 +0300

    fix build config

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
-  base: "/E-commerce-website/",
-});
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
