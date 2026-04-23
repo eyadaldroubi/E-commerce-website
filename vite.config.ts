4a2a19cb087944dffd56a9f73b98cff2594be2a7
EYAD <eyadaldroubi@gmail.com>
2026-04-23 14:55:41 +0300

    update dist build

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,15 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
+      base: "/E-commerce-website/",
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
