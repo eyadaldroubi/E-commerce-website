2b5400ff06be300888184ab0f2b297e160a12b2b
EYAD <eyadaldroubi@gmail.com>
2026-04-23 15:06:24 +0300

    fix github pages path

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig, loadEnv } from 'vite'
 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '')
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react()],
-      base: '/E-commerce-website/',
+    plugins: [react(), tailwindcss()],
+    base: '/E-commerce-website/',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
@@ -19,10 +17,5 @@ export default defineConfig(({mode}) => {
         '@': path.resolve(__dirname, '.'),
       },
     },
-    server: {
-      // HMR is disabled in AI Studio via DISABLE_HMR env var.
-      // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-  };
-});
+  }
+})
